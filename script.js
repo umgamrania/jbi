81dc0a22ac138868da215f190091781df4cccab8
Umang <umangamrania@outlook.com>
2024-12-05 12:27:28 +0530

    Added product page & changed directory structure

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
-const input = [
-    document.querySelector("#r1"),
-    document.querySelector("#r2"),
-    document.querySelector("#r3"),
-    document.querySelector("#r4"),
-    document.querySelector("#r5")
-]
+// Mobile menu
+const menuBtn = document.querySelector(".menu-btn")
+const linksContainer = document.querySelector(".links-container")
+let menuState = 0;
 
-const font = document.querySelector(".heading")
-
-let a = 0, b = 0, c = 0, d = 0, f = 0
-
-input[0].oninput = e => {
-    console.log(e.target.value);
-    a = e.target.value
-    font.style.setProperty("font-variation-settings", `"MONO" ${a}, "CASL" ${b}, "CRSV" ${c}, "wght" ${d}, "slnt" ${f}`)
+menuBtn.onclick = () => {
+    linksContainer.classList.toggle("open")
 }
 
-input[1].oninput = e => {
-    b = e.target.value
-    font.style.setProperty("font-variation-settings", `"MONO" ${a}, "CASL" ${b}, "CRSV" ${c}, "wght" ${d}, "slnt" ${f}`)
-}
+const links = document.querySelectorAll(".link:not(:has(.sub-menu))")
+links.forEach(link => {
+    link.onclick = () => {
+        linksContainer.classList.remove("open")
+    }
+})
 
-input[2].oninput = e => {
-    c = e.target.value
-    font.style.setProperty("font-variation-settings", `"MONO" ${a}, "CASL" ${b}, "CRSV" ${c}, "wght" ${d}, "slnt" ${f}`)
-}
-
-input[3].oninput = e => {
-    d = e.target.value
-    font.style.setProperty("font-variation-settings", `"MONO" ${a}, "CASL" ${b}, "CRSV" ${c}, "wght" ${d}, "slnt" ${f}`)
-}
-
-input[4].oninput = e => {
-    f = e.target.value
-    font.style.setProperty("font-variation-settings", `"MONO" ${a}, "CASL" ${b}, "CRSV" ${c}, "wght" ${d}, "slnt" ${f}`)
+// nav bar scroll behaviour
+const nav = document.querySelector(".nav-bar")
+window.onscroll = () => {
+    if(window.scrollY > 0) {
+        nav.classList.add("scrolled")
+    } else {
+        nav.classList.remove("scrolled")
+    }
 }
